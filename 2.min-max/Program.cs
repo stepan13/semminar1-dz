ad7db248ea718e1b349b0edb0ff325a493ef14ff
Vanger <stepan13@gmail.com>
2022-11-13 18:24:38 +0000

    задача 2

---
+++ 2.min-max/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("input number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2)
+    Console.WriteLine("numbers are equal");
+else if (num1 > num2)
+    Console.WriteLine("max = " + num1 + " min = " + num2);
+else
+    Console.WriteLine("max = " + num2 + " min = " + num1);
+
