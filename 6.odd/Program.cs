766905826b8aa5c1f27e89783785dd024ab434cf
Vanger <stepan13@gmail.com>
2022-11-13 18:33:39 +0000

    задача 6

---
+++ 6.odd/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("input number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+    Console.WriteLine("number is even");
+else
+    Console.WriteLine("number is odd");
