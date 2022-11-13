d302f9a5e165d0f06c5a89409ca88718cb7d47b4
Vanger <stepan13@gmail.com>
2022-11-13 18:28:21 +0000

    задача 4

---
+++ 4.maxOfThree/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("input number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+    max = num2;
+if (num3 > max)
+    max = num3;
+
+Console.WriteLine("max = " + max);
+
