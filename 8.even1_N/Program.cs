e2a8a9d8d32cc5c021e90ef9109e703ed1af4383
Vanger <stepan13@gmail.com>
2022-11-13 18:38:10 +0000

    задача 8

---
+++ 8.even1_N/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("input number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+while (index <= num1)
+{
+    if (index % 2 == 0)
+        Console.Write(index + " ");
+    index++;
+}
